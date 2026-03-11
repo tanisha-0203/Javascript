294c96df4a4c4f16c8636915f282fd65b4a62d36
tanisha-0203 <158428837+tanisha-0203@users.noreply.github.com>
2026-03-11 12:05:27 +0000

    Data Types

---
+++ Basics/SummaryDT.js
@@ -0,0 +1,22 @@
+//NON PRIMTIVE
+//array//
+const arr=["apple","mango","banana"];
+let obj={
+    name:"tanisha",
+    age:21
+}
+
+//return type of data types
+// 1)Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
